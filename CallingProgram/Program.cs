0408e00f8f98295c07f7ead0228cbd12b71c012f
Daniel Vinci <me@danielvinci.com>
2022-12-24 20:24:32 -0700

    add CallingProgram

---
+++ CallingProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+public static partial class Program
+{
+    [LibraryImport(@"C:\src\DotNet-NativeAOT-Library-Example", EntryPoint = "native_add")]
+    private static partial int Add(int a, int b);
+
+    public static void Main()
+    {
+        Console.WriteLine(Add(500, 36));
+    }
+}
