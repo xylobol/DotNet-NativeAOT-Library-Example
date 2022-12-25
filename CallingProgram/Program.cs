3c2d0964ccc552698e673056d9b9ea6dd0f7470f
Daniel Vinci <me@danielvinci.com>
2022-12-24 20:28:47 -0700

    some more work on the callingprogram

--- CallingProgram/Program.cs
+++ CallingProgram/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System.Runtime.InteropServices;
 
+namespace CallingProgram;
+
 public static partial class Program
 {
-    [LibraryImport(@"C:\src\DotNet-NativeAOT-Library-Example", EntryPoint = "native_add")]
+    private const string ProjectRoot = @"C:\src\DotNet-NativeAOT-Library-Example";
+    
+    [LibraryImport($"{ProjectRoot}/ExampleLibrary/bin/Release/net7.0/win-x64/native/ExampleLibrary.dll", EntryPoint = "native_add")]
     private static partial int Add(int a, int b);
 
     public static void Main()
