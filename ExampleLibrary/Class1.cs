d0855a08d20b1b3f3e3f1f2a431ed39cc547f750
Daniel Vinci <me@danielvinci.com>
2022-12-24 20:21:09 -0700

    add ExampleLibrary

---
+++ ExampleLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ExampleLibrary;
+
+public class Class1
+{
+    [UnmanagedCallersOnly(EntryPoint = "native_add")]
+    public static int Add(int a, int b) => a + b;
+}
